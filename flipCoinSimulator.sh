abaefafa61c47c6bb5242534c92f62bb240ab3c7
bhagyashree98 <bnsingasane@gmail.com>
2020-03-23 23:48:05 +0530

    Created simulation for one coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
 
 r=$((RANDOM % 2))
-        if [ $r -eq 1 ]
-        then
-                echo heads
-        else
-                echo tails
-        fi
+echo "enter 1 for head and 0 for tails"
+read choice 
+if [ $r -eq 1 ]
+then 
+	 echo heads
+else 
+	echo tails
+fi
+
+if [ $r -eq $choice ]
+then 
+	echo "user won"
+else
+	echo "user lost"
+fi
